a715a8097acf24245e67dc803bd6dbf620c92507
theveryoilydill <theveryoilydill@gmail.com>
2026-06-03 00:05:03 +0000

    Fix the Format

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -4,12 +4,12 @@ import { createHashRouter, UNSAFE_createClientRoutes, UNSAFE_FrameworkContext } 
 import { RouterProvider } from "react-router/dom";
 
 declare global {
-  interface Window {
-    __reactRouterContext: any;
-    __reactRouterDataRouter?: unknown;
-    __reactRouterManifest: any;
-    __reactRouterRouteModules: any;
-  }
+	interface Window {
+		__reactRouterContext: any;
+		__reactRouterDataRouter?: unknown;
+		__reactRouterManifest: any;
+		__reactRouterRouteModules: any;
+	}
 }
 
 const context = window.__reactRouterContext;
@@ -17,39 +17,39 @@ const manifest = window.__reactRouterManifest;
 const routeModules = window.__reactRouterRouteModules;
 
 const routes = UNSAFE_createClientRoutes(
-  manifest.routes,
-  routeModules,
-  context.state,
-  context.ssr,
-  context.isSpaMode,
+	manifest.routes,
+	routeModules,
+	context.state,
+	context.ssr,
+	context.isSpaMode,
 );
 
 const router = createHashRouter(routes, {
-  basename: context.basename,
-  hydrationData: context.state,
-  future: {
-    v8_passThroughRequests: context.future.v8_passThroughRequests,
-  },
+	basename: context.basename,
+	hydrationData: context.state,
+	future: {
+		v8_passThroughRequests: context.future.v8_passThroughRequests,
+	},
 });
 
 window.__reactRouterDataRouter = router;
 
 startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <UNSAFE_FrameworkContext.Provider
-        value={{
-          manifest,
-          routeModules,
-          future: context.future,
-          ssr: context.ssr,
-          isSpaMode: context.isSpaMode,
-          routeDiscovery: context.routeDiscovery,
-        }}
-      >
-        <RouterProvider router={router} />
-      </UNSAFE_FrameworkContext.Provider>
-    </StrictMode>,
-  );
+	hydrateRoot(
+		document,
+		<StrictMode>
+			<UNSAFE_FrameworkContext.Provider
+				value={{
+					manifest,
+					routeModules,
+					future: context.future,
+					ssr: context.ssr,
+					isSpaMode: context.isSpaMode,
+					routeDiscovery: context.routeDiscovery,
+				}}
+			>
+				<RouterProvider router={router} />
+			</UNSAFE_FrameworkContext.Provider>
+		</StrictMode>,
+	);
 });
--- react-router.config.ts
+++ react-router.config.ts
@@ -1,14 +1,14 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  ssr: false,
-  prerender: ["/"],
-  future: {
-    v8_middleware: true,
-    v8_passThroughRequests: true,
-    v8_splitRouteModules: true,
-    v8_trailingSlashAwareDataRequests: true,
-    v8_viteEnvironmentApi: true,
-  },
+	// Config options...
+	ssr: false,
+	prerender: ["/"],
+	future: {
+		v8_middleware: true,
+		v8_passThroughRequests: true,
+		v8_splitRouteModules: true,
+		v8_trailingSlashAwareDataRequests: true,
+		v8_viteEnvironmentApi: true,
+	},
 } satisfies Config;
