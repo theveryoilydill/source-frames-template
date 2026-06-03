21ebd774714eb9d47a6033042b057bd41d01c2ab
theveryoilydill <theveryoilydill@gmail.com>
2026-06-03 03:54:25 +0000

    Fix the Base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+	base: "./", // IMPORTANT for Github Pages because it looks at the base of your pages (e.g., https://username.github.io/) to find stuff from index.html.
 	plugins: [tailwindcss(), reactRouter()],
 	resolve: {
 		tsconfigPaths: true,
