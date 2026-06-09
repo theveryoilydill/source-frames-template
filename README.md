23954231d60db7a97c8ec5a5065ea2fc9cd98c79
theveryoilydill <theveryoilydill@gmail.com>
2026-06-09 20:54:40 +0000

    Fixed It by Removing Hash Router from Entry.Client.TSX

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Source Frames!
 
 This is a template for a react router app using hash router to build to html files only with no backend needed. You can add a customizable array of frames going to different URLS or doing different things.
+
+## Notes
+
+- It is recommended to use Cloudflare Pages, Vercel, or Netlify instead of github pages because this project uses react router framework mode
--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,55 +1,12 @@
+import { HydratedRouter } from "react-router/dom";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
-import { createHashRouter, UNSAFE_createClientRoutes, UNSAFE_FrameworkContext } from "react-router";
-import { RouterProvider } from "react-router/dom";
-
-declare global {
-	interface Window {
-		__reactRouterContext: any;
-		__reactRouterDataRouter?: unknown;
-		__reactRouterManifest: any;
-		__reactRouterRouteModules: any;
-	}
-}
-
-const context = window.__reactRouterContext;
-const manifest = window.__reactRouterManifest;
-const routeModules = window.__reactRouterRouteModules;
-
-const routes = UNSAFE_createClientRoutes(
-	manifest.routes,
-	routeModules,
-	context.state,
-	context.ssr,
-	context.isSpaMode,
-);
-
-const router = createHashRouter(routes, {
-	basename: context.basename,
-	hydrationData: context.state,
-	future: {
-		v8_passThroughRequests: context.future.v8_passThroughRequests,
-	},
-});
-
-window.__reactRouterDataRouter = router;
 
 startTransition(() => {
-	hydrateRoot(
-		document,
-		<StrictMode>
-			<UNSAFE_FrameworkContext.Provider
-				value={{
-					manifest,
-					routeModules,
-					future: context.future,
-					ssr: context.ssr,
-					isSpaMode: context.isSpaMode,
-					routeDiscovery: context.routeDiscovery,
-				}}
-			>
-				<RouterProvider router={router} />
-			</UNSAFE_FrameworkContext.Provider>
-		</StrictMode>,
-	);
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <HydratedRouter />
+    </StrictMode>
+  );
 });
