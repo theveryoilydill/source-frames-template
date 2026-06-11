489f1bd2d7e12194921c8bdf9ab1729d8b78fe59
theveryoilydill <theveryoilydill@gmail.com>
2026-06-11 03:27:19 +0000

    Fixed Cloudflare
    * It doesn't work with github pages now, and probably forever.
    * Main hosters will now be Cloudflare and what ever other hosters allow SSR.
    * Also openning settings doesn't work sometimes in wrangler for some reason.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	base: "./", // IMPORTANT for Github Pages because it looks at the base of your pages (e.g., https://username.github.io/) to find stuff from index.html.
+	base: "/", // If you want to put it somewhere like username.github.io/repo, you need to put ./ here instead of /
 	plugins: [tailwindcss(), reactRouter()],
 	resolve: {
 		tsconfigPaths: true,
