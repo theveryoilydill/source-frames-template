cab2f975bb6eee5596aa2cfcbabf4d17750ccaf8
theveryoilydill <theveryoilydill@gmail.com>
2026-06-13 00:38:06 +0000

    Fix cloudflare issues
    * Added new things to the gitignore
    * Stole stuff from the react router cloudflare template

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	base: "/", // If you want to put it somewhere like username.github.io/repo, you need to put ./ here instead of /
-	plugins: [tailwindcss(), reactRouter()],
+	plugins: [
+		tailwindcss(),
+		reactRouter(),
+	],
 	resolve: {
 		tsconfigPaths: true,
 	},
