f8530d0f8957e8e2e65b4efd0bd553463a1d533d
theveryoilydill <theveryoilydill@gmail.com>
2026-06-13 19:04:11 +0000

    Try to fix cloudflare deploy again

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	base: "/", // If you want to put it somewhere like username.github.io/repo, you need to put ./ here instead of /
-	plugins: [tailwindcss(), reactRouter()],
-	resolve: {
-		tsconfigPaths: true,
-	},
+	base: "/", // If you want to put it somewhere like username.github.io/repo, you need to put './' here instead of '/'
+  plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    tailwindcss(),
+    reactRouter(),
+  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
