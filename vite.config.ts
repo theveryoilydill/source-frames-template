f2460dc6dd33e101a3ecb3f8e724f60ff06cb3a5
theveryoilydill <theveryoilydill@gmail.com>
2026-06-13 18:44:02 +0000

    Update Some Files

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	base: "/", // If you want to put it somewhere like username.github.io/repo, you need to put ./ here instead of /
-	plugins: [
-		tailwindcss(),
-		reactRouter(),
-	],
+	plugins: [tailwindcss(), reactRouter()],
 	resolve: {
 		tsconfigPaths: true,
 	},
--- workers/app.ts
+++ workers/app.ts
@@ -1,12 +1,12 @@
 import { createRequestHandler } from "react-router";
 
 const requestHandler = createRequestHandler(
-  () => import("virtual:react-router/server-build"),
-  import.meta.env.MODE,
+	() => import("virtual:react-router/server-build"),
+	import.meta.env.MODE,
 );
 
 export default {
-  async fetch(request) {
-    return requestHandler(request);
-  },
+	async fetch(request) {
+		return requestHandler(request);
+	},
 } satisfies ExportedHandler<Env>;
